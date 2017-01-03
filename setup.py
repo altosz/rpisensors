a97ffbb7467c6164b4401bf8c7494dcf061867b3
Alexei Tishin <altos.z@gmail.com>
2017-01-03 13:34:06 +0300

    add readme stub to package

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup
 
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name="rpisensors",
     version="0.1.0",
-    descriptions="Raspberry Pi Sensors",
+    description="Raspberry Pi Sensors",
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Topic :: System :: Hardware',
+        'Topic :: Software Development :: Embedded Systems',
+    ],
     url="https://github.com/altosz/rpisensors",
     author="Alexei Tishin",
     author_email="altos.z@gmail.com",
